8acf9aa2a7287e4fa4c8544845e55e6ab73eba06
Eduardo Lanzini <eduardolanzini91@gmail.com>
2021-03-09 17:37:33 -0300

    Composer update

--- app/views/index.php
+++ app/views/index.php
@@ -5,7 +5,7 @@
 <h3>Hello World!</h3>
 
 <style type="text/css">
-    h3{text-align: center;margin:auto;margin-top: 40vh;font-size: 50px;}
+    h3{text-align: center;margin:auto;margin-top: 40vh;font-size: 50px;color: #0D0D0D}
 </style>
 
 <?php self::end('content'); ?>
